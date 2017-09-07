5034ca296721c5099180aace87e17d8def569072
Linden van der, Marco <Marco.G.van.der.Linden@rabobank.nl>
2017-09-07 22:55:32 +0200

    added script 3

---
+++ scripts/test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# param 1: docker compose file that will be updated
+# param 2: nexus url with new version of image
+if [  $# -le 2 ]
+  then
+		echo "[Usage: test.sh p1 p2]\n"
+		exit 1
+fi
+
+echo "P1: $1"
+echo "P2: $2"
