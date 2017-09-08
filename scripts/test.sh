d68a4140dc6ff1ea003e544dc1bf9eef599d4995
Linden van der, Marco <Marco.G.van.der.Linden@rabobank.nl>
2017-09-08 10:31:24 +0200

    added script 5

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,11 +2,11 @@
 
 # param 1: docker compose file that will be updated
 # param 2: nexus url with new version of image
-if [  $# -le 2 ]
-  then
-		echo "[Usage: test.sh p1 p2]\n"
-		exit 1
-fi
-
+#if [  $# -le 2 ]
+#  then
+#		echo "[Usage: test.sh p1 p2]
+#		exit 1
+#fi
+echo "Run script"
 echo "P1: $1"
 echo "P2: $2"
